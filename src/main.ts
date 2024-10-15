8098c85015f0e8a09436a4ee2dd3f28eb7bf76f1
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-15 09:51:38 +0330

    add line to show address of running server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Start Server
+  const PORT = +3000;
+  await app.listen(PORT, () => {
+    console.log(`Root:    http://localhost:${PORT}`);
+    // console.log(`Swagger: http://localhost:${PORT}/swagger`);
+  });
 }
+
 bootstrap();
