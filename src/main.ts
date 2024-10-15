5ea7b503085894a646f9906fbfc0304ed4ba23d0
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-15 12:50:43 +0330

    add validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import * as cookieParser from "cookie-parser";
 import { SwaggerConfigInit } from "./config/swagger.config";
+import { ValidationPipe } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Configure Swagger
   SwaggerConfigInit(app);
 
+  // Activate Validation
+  app.useGlobalPipes(new ValidationPipe());
+
   // Start Server
-  // const PORT = +3000;
   const { PORT } = process.env;
   await app.listen(PORT, () => {
     console.log(`Root:    http://localhost:${PORT}`);
