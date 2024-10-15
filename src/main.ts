6a06f4b8294c95a6ca553738080b6b47efa29f46
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-15 09:53:51 +0330

    add and configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
+import { SwaggerConfigInit } from "./config/swagger.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure Swagger
+  SwaggerConfigInit(app);
 
   // Start Server
   const PORT = +3000;
   await app.listen(PORT, () => {
     console.log(`Root:    http://localhost:${PORT}`);
-    // console.log(`Swagger: http://localhost:${PORT}/swagger`);
+    console.log(`Swagger: http://localhost:${PORT}/swagger`);
   });
 }
 
