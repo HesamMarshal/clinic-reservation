0d24980dfc2808b9beac21da962747784e506c38
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-15 10:05:07 +0330

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { join } from "path";
 
+import { UserModule } from "./user/user.module";
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: join(process.cwd(), ".env"),
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
