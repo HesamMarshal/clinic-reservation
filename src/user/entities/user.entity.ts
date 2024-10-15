1e3fbc6ed8c90873842613050e4aad1419cec2e7
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-15 10:12:09 +0330

    add user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { EntityName } from "src/common/enums/entity.enum";
+import { BaseEntity, Column, Entity } from "typeorm";
+
+@Entity(EntityName.User)
+export class UserEntity extends BaseEntity {
+  // Fileds
+
+  @Column({})
+  first_name: string;
+
+  @Column({})
+  last_name: string;
+
+  @Column({ unique: true })
+  mobile: string;
+}
