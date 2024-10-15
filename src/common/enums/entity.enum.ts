b69950a6b4894eda0d058d6bb6221b9bb8ac874e
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-15 10:09:30 +0330

    add enums for entities

---
+++ src/common/enums/entity.enum.ts
@@ -0,0 +1,9 @@
+export enum EntityName {
+  Otp = "otp",
+  User = "user",
+  Clinic = "clinic",
+  Category = "category",
+  Transacion = "transaction",
+  Reservaion = "reservation",
+  Planner = "planner",
+}
