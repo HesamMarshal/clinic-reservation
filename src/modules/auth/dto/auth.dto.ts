1cf6aaec8730fe3575f32588e970455ac31de8e4
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-15 12:52:12 +0330

    add AuthDto and CheckOtpDto

---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsString, Length } from "class-validator";
+import { AuthType } from "../enums/type.enum";
+import { UserType } from "../enums/userType.enum";
+
+export class AuthDto {
+  @ApiProperty()
+  @IsString()
+  @Length(11)
+  mobile: string;
+
+  // @ApiProperty({ enum: AuthType })
+  // @IsEnum(AuthType)
+  // type: string;
+
+  // @ApiProperty({ enum: UserType })
+  // @IsEnum(UserType)
+  // userType: UserType;
+}
+
+export class CheckOtpDto {
+  @ApiProperty()
+  @IsString()
+  @Length(5, 5)
+  code: string;
+}
