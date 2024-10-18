ae3b22d0b22a981c99865c67f695596f8cbcb289
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-18 16:18:21 +0330

    change order of modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { join } from "path";
-
 import { UserModule } from "./modules/user/user.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TypeOrmConfig } from "./config/typeorm.config";
-import { ClinicModule } from './modules/clinic/clinic.module';
+import { ClinicModule } from "./modules/clinic/clinic.module";
 
 @Module({
   imports: [
@@ -17,8 +16,8 @@ import { ClinicModule } from './modules/clinic/clinic.module';
       envFilePath: join(process.cwd(), ".env"),
     }),
     TypeOrmModule.forRoot(TypeOrmConfig()),
-    UserModule,
     AuthModule,
+    UserModule,
     ClinicModule,
   ],
   controllers: [AppController],
