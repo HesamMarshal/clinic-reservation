730a600fc762aa070f2f6a8009be55073ce0054e
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-18 16:18:48 +0330

    add authGuard

--- src/modules/clinic/clinic.controller.ts
+++ src/modules/clinic/clinic.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ClinicService } from './clinic.service';
-import { CreateClinicDto } from './dto/create-clinic.dto';
-import { UpdateClinicDto } from './dto/update-clinic.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from "@nestjs/common";
+import { ClinicService } from "./clinic.service";
+import { CreateClinicDto } from "./dto/create-clinic.dto";
+import { UpdateClinicDto } from "./dto/update-clinic.dto";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "../auth/guards/auth.guard";
 
-@Controller('clinic')
+@Controller("clinic")
+@ApiTags("Clinic")
+@ApiBearerAuth("Authorization")
+@UseGuards(AuthGuard)
 export class ClinicController {
   constructor(private readonly clinicService: ClinicService) {}
 
@@ -17,18 +31,18 @@ export class ClinicController {
     return this.clinicService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.clinicService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClinicDto: UpdateClinicDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateClinicDto: UpdateClinicDto) {
     return this.clinicService.update(+id, updateClinicDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.clinicService.remove(+id);
   }
 }
