d26ae0b0ecf4aecbe83088cf907275195b5d0a59
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-18 11:03:29 +0330

    add clinicId to AccessTokenPayload

--- src/modules/auth/types/payloads.ts
+++ src/modules/auth/types/payloads.ts
@@ -1,9 +1,11 @@
 export type CookiePayload = {
   userId: number;
+  clinicId: number;
 };
 
 export type AccessTokenPayload = {
   userId: number;
+  clinicId: number;
 };
 
 export type PhoneTokenPayload = {
