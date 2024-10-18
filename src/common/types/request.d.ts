8246ad6cbc96a90d3a932c02a9221501bb1a75d0
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-18 12:47:22 +0330

    add clinic to request

--- src/common/types/request.d.ts
+++ src/common/types/request.d.ts
@@ -1,9 +1,11 @@
+import { ClinicEntity } from "src/modules/clinic/entities/clinic.entity";
 import { UserEntity } from "src/modules/user/entities/user.entity";
 
 declare global {
   namespace Express {
     interface Request {
       user?: UserEntity;
+      clinic?: ClinicEntity;
     }
   }
 }
