c729147c6d615481225d1b883cb53a4c045bd2ac
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-18 11:02:23 +0330

    change mobile to nullable

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -18,7 +18,7 @@ export class UserEntity extends BaseEntity {
   @Column({ nullable: true })
   last_name: string;
 
-  @Column({ unique: true })
+  @Column({ nullable: true })
   mobile: string;
 
   @Column({ nullable: true })
