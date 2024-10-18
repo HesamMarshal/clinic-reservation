f1cd8796139b18a358d9c16f8b38889715bc1944
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-18 16:17:51 +0330

    add some modules and entities

--- src/modules/clinic/clinic.module.ts
+++ src/modules/clinic/clinic.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
-import { ClinicService } from './clinic.service';
-import { ClinicController } from './clinic.controller';
+import { Module } from "@nestjs/common";
+import { ClinicService } from "./clinic.service";
+import { ClinicController } from "./clinic.controller";
+import { AuthModule } from "../auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "../user/entities/user.entity";
+import { OtpEntity } from "../user/entities/otp.entity";
+import { ClinicEntity } from "./entities/clinic.entity";
 
 @Module({
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([UserEntity, OtpEntity, ClinicEntity]),
+  ],
   controllers: [ClinicController],
   providers: [ClinicService],
 })
