b453791c57896f95e53743bb0a12f50fa3d717f1
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-19 11:31:42 +0330

    add categoryEntity

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,1 +1,18 @@
-export class Category {}
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntityName } from "src/common/enums/entity.enum";
+import { Column, Entity } from "typeorm";
+
+@Entity(EntityName.Category)
+export class CategoryEntity extends BaseEntity {
+  @Column()
+  title: string;
+
+  @Column()
+  slug: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ nullable: true })
+  image: string;
+}
