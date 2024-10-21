60009399f7d64cacbe14397c956dca7919d8d0e5
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-21 10:14:54 +0330

    add toLowercase

--- src/common/utils/functions.util.ts
+++ src/common/utils/functions.util.ts
@@ -1,7 +1,8 @@
 export const createSlug = (str: string) => {
   return str
     ?.replace(/[،:ًٌٍ\.\+\-_)(*&^%$#@!?;:><]+/g, "")
-    ?.replace(/[\s]+/g, "-");
+    ?.replace(/[\s]+/g, "-")
+    .toLowerCase();
 };
 
 export const randomId = () => Math.random().toString(36).substring(2);
