f766ba1ebf5c50d8d79587eeecdf11806b18f1f4
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-22 11:15:01 +0330

    add decorator

---
+++ src/common/decorators/upload-file.decorator.ts
@@ -0,0 +1,10 @@
+import { ParseFilePipe, UploadedFiles, applyDecorators } from "@nestjs/common";
+
+export function UploadedOptionalFiles() {
+  return UploadedFiles(
+    new ParseFilePipe({
+      fileIsRequired: false,
+      validators: [],
+    })
+  );
+}
