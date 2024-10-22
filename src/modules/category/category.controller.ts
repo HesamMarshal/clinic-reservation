b30cfad2026f01019209268219745253546838ad
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-22 13:52:59 +0330

    get file from request

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -6,12 +6,17 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from "@nestjs/common";
 import { CategoryService } from "./category.service";
 import { CreateCategoryDto } from "./dto/create-category.dto";
 import { UpdateCategoryDto } from "./dto/update-category.dto";
 import { ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { SwaggerConsumes } from "src/common/swagger-consume.enum";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { multerStorage } from "src/common/utils/multer.util";
+import { CategoryImages } from "./types/files.type";
+import { UploadedOptionalFiles } from "src/common/decorators/upload-file.decorator";
 
 @Controller("category")
 @ApiTags("Category")
@@ -19,9 +24,17 @@ export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  @ApiConsumes(SwaggerConsumes.UrlEncoded, SwaggerConsumes.Json)
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoryService.create(createCategoryDto);
+  @ApiConsumes(SwaggerConsumes.MultipartData)
+  @UseInterceptors(
+    FileFieldsInterceptor([{ name: "image", maxCount: 1 }], {
+      storage: multerStorage("category-image"),
+    })
+  )
+  create(
+    @UploadedOptionalFiles() file: CategoryImages,
+    @Body() createCategoryDto: CreateCategoryDto
+  ) {
+    return this.categoryService.create(file, createCategoryDto);
   }
 
   @Get()
