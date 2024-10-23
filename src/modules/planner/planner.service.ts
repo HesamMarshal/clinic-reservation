aaee93b090ac6b48e5d95eb637f72dd1af50334c
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-23 16:55:00 +0330

    add findAll and findOne

--- src/modules/planner/planner.service.ts
+++ src/modules/planner/planner.service.ts
@@ -1,13 +1,26 @@
-import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { CreatePlannerDto } from "./dto/create-planner.dto";
 import { UpdatePlannerDto } from "./dto/update-planner.dto";
 import { REQUEST } from "@nestjs/core";
 import { Request } from "express";
-import { AuthMessage } from "../auth/enums/message.enum";
+import { AuthMessage, NotFoundMessage } from "../auth/enums/message.enum";
+import { InjectRepository } from "@nestjs/typeorm";
+import { PlannerEntity } from "./entities/planner.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class PlannerService {
-  constructor(@Inject(REQUEST) private request: Request) {}
+  constructor(
+    @InjectRepository(PlannerEntity)
+    private plannerRepository: Repository<PlannerEntity>,
+
+    @Inject(REQUEST) private request: Request
+  ) {}
   create(createPlannerDto: CreatePlannerDto) {
     // if the logged in user is not a clinic can work on category
     const { clinic } = this?.request;
@@ -15,18 +28,21 @@ export class PlannerService {
     return "This action adds a new planner";
   }
 
-  findAll() {
-    // if the logged in user is not a clinic can work on category
-    const { clinic } = this?.request;
-    if (!clinic) throw new UnauthorizedException(AuthMessage.ClinicLogin);
-    return `This action returns all planner`;
+  async findAll() {
+    const plans = await this.plannerRepository.find();
+    if (!plans) throw new NotFoundException(NotFoundMessage.PlanNotFound);
+    return plans;
   }
 
-  findOne(id: number) {
+  async findOne() {
     // if the logged in user is not a clinic can work on category
     const { clinic } = this?.request;
     if (!clinic) throw new UnauthorizedException(AuthMessage.ClinicLogin);
-    return `This action returns a #${id} planner`;
+
+    const { id } = clinic;
+    const plan = await this.plannerRepository.findOneBy({ id });
+    if (!plan) throw new NotFoundException(NotFoundMessage.PlanNotFound);
+    return plan;
   }
 
   update(id: number, updatePlannerDto: UpdatePlannerDto) {
