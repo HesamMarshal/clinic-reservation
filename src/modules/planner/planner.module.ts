476298843f50fa39a99c8a7492713e33e3b1fd15
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-23 16:55:25 +0330

    import plannerEntity and AuthModule

--- src/modules/planner/planner.module.ts
+++ src/modules/planner/planner.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { PlannerService } from "./planner.service";
 import { PlannerController } from "./planner.controller";
 import { AuthModule } from "../auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { PlannerEntity } from "./entities/planner.entity";
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forFeature([PlannerEntity])],
   controllers: [PlannerController],
   providers: [PlannerService],
 })
