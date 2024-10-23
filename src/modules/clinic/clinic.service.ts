8ab1b9d200f6aa554fa13f28ff7c8baef589fdd4
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-23 12:39:20 +0330

    update the clinic

--- src/modules/clinic/clinic.service.ts
+++ src/modules/clinic/clinic.service.ts
@@ -15,12 +15,14 @@ import {
   PublicMessage,
 } from "../auth/enums/message.enum";
 import { Request } from "express";
+import { CategoryService } from "../category/category.service";
 
 @Injectable()
 export class ClinicService {
   constructor(
     @InjectRepository(ClinicEntity)
     private clinicRepository: Repository<ClinicEntity>,
+    private readonly categoryService: CategoryService,
 
     @Inject(REQUEST) private request: Request
   ) {}
@@ -43,17 +45,43 @@ export class ClinicService {
 
   async update(updateClinicDto: UpdateClinicDto) {
     //  get user from token
-    const { first_name, last_name, address, mobile_no } = updateClinicDto;
+    const { first_name, last_name, address, mobile_no, category } =
+      updateClinicDto;
+
     const { clinic } = this?.request;
     if (!clinic) throw new UnauthorizedException(AuthMessage.LoginAgain);
     const { id } = clinic;
-    const user = await this.clinicRepository.findOneBy({ id });
+    const oldClinic = await this.clinicRepository.findOneBy({ id });
+    if (!oldClinic) throw new NotFoundException(NotFoundMessage.ClinicNotFount);
 
-    if (!user) throw new NotFoundException(NotFoundMessage.ClinicNotFount);
+    if (first_name) oldClinic.first_name = first_name;
+    if (last_name) oldClinic.last_name = last_name;
+    if (address) oldClinic.address = address;
+    if (mobile_no) oldClinic.mobile_no = mobile_no;
 
+    if (category) {
+      const existCategory =
+        await this.categoryService.checkCategoryBySlug(category);
+      if (existCategory) {
+        oldClinic.categoryId = existCategory.id;
+      } else {
+        // create new category and add to oldClinic
+        const newCategory =
+          await this.categoryService.createWithTitle(category);
+        oldClinic.categoryId = newCategory.id;
+      }
+    }
+
+    //  edit
     await this.clinicRepository.update(
       { id },
-      { first_name, last_name, address, mobile_no }
+      {
+        first_name: oldClinic.first_name,
+        last_name: oldClinic.last_name,
+        address: oldClinic.address,
+        mobile_no: oldClinic.mobile_no,
+        categoryId: oldClinic.categoryId,
+      }
     );
     return { message: PublicMessage.Updated };
   }
