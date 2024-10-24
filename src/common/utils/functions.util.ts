f4287e025cb1968ea190149918575e33ec574c60
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-24 11:48:17 +0330

    add isTrue and isFalse

--- src/common/utils/functions.util.ts
+++ src/common/utils/functions.util.ts
@@ -6,3 +6,6 @@ export const createSlug = (str: string) => {
 };
 
 export const randomId = () => Math.random().toString(36).substring(2);
+
+export const isTrue = (value) => ["true", "True", 1, true].includes(value);
+export const isFalse = (value) => ["false", "False", 0, false].includes(value);
