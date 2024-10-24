5da416188a9af6fccaf1b68f371e1879de8c6e8e
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-24 11:11:32 +0330

    create planner dto

--- src/modules/planner/dto/create-planner.dto.ts
+++ src/modules/planner/dto/create-planner.dto.ts
@@ -1,1 +1,22 @@
-export class CreatePlannerDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+import { IsEnum } from "class-validator";
+import { DayName } from "../types/dayName.enum";
+
+export class CreatePlannerDto {
+  @ApiProperty({ enum: DayName })
+  @IsEnum(DayName)
+  dayName: string;
+
+  @ApiProperty()
+  day_number: number;
+
+  @ApiProperty()
+  start_time: number;
+
+  @ApiProperty()
+  finish_time: number;
+
+  @ApiProperty()
+  status: boolean;
+}
