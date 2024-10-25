e796c03b78be99dd776db5c4b443f37746ce7f87
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 14:20:04 +0330

    add transaction dto

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTransactionDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsNumber } from "class-validator";
+
+export class CreateTransactionDto {
+  @ApiProperty()
+  //   @IsNumber()
+  clinicId: number;
+
+  @ApiProperty()
+  //   @IsNumber()
+  amount: number;
+
+  @ApiProperty()
+  status: boolean;
+
+  @ApiProperty()
+  //   @IsDate()
+  date: Date;
+}
