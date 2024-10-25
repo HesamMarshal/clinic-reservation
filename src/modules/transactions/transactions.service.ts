1c9e1beb40839d7652b3bfb943f084861c1a1c96
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 14:22:10 +0330

    implement methods for endpoints

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -1,33 +1,81 @@
-import { Injectable } from "@nestjs/common";
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { CreateTransactionDto } from "./dto/create-transaction.dto";
 import { UpdateTransactionDto } from "./dto/update-transaction.dto";
 import { TransactionEntity } from "./entities/transaction.entity";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
+import { REQUEST } from "@nestjs/core";
+import { Request } from "express";
+import {
+  AuthMessage,
+  NotFoundMessage,
+  PublicMessage,
+} from "../auth/enums/message.enum";
+import { ClinicEntity } from "../clinic/entities/clinic.entity";
+import { ClinicService } from "../clinic/clinic.service";
 
 @Injectable()
 export class TransactionsService {
   constructor(
     @InjectRepository(TransactionEntity)
-    private transactionRepository: Repository<TransactionEntity>
+    private transactionRepository: Repository<TransactionEntity>,
+
+    private readonly clinicService: ClinicService,
+
+    @Inject(REQUEST) private request: Request
   ) {}
-  create(createTransactionDto: CreateTransactionDto) {
-    return "This action adds a new transaction";
+  async create(createTransactionDto: CreateTransactionDto) {
+    // only users can make a transaction
+    const { user } = this?.request;
+    if (!user) throw new UnauthorizedException(AuthMessage.UserLogin);
+    const { id: userId } = user;
+
+    const { clinicId, amount, date, status } = createTransactionDto;
+    await this.clinicService.existClinic(clinicId);
+
+    await this.transactionRepository.insert({
+      userId,
+      clinicId,
+      amount,
+      status,
+      date,
+    });
+
+    return {
+      message: PublicMessage.Created,
+    };
   }
 
-  findAll() {
-    return `This action returns all transactions`;
+  async findAll() {
+    const transactions = await this.transactionRepository.find();
+    return transactions;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
+    // only users can make a transaction
+    const { user } = this?.request;
+    if (!user) throw new UnauthorizedException(AuthMessage.UserLogin);
+    const { id: userId } = user;
     return `This action returns a #${id} transaction`;
   }
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
+  async findMyTransactions() {
+    // only users can make a transaction
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+    const { user } = this?.request;
+    if (!user) throw new UnauthorizedException(AuthMessage.UserLogin);
+    const { id: userId } = user;
+
+    const [transactions] = await this.transactionRepository.find({
+      where: { userId },
+    });
+    if (!transactions)
+      throw new NotFoundException(NotFoundMessage.TransacionNotFound);
+    return transactions;
   }
 }
