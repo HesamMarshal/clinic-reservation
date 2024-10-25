a546689dd64e35868fa9c92a2b217998c9c712ec
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 20:16:02 +0330

    add AuthModule and reservationEntity

--- src/modules/reservation/reservation.module.ts
+++ src/modules/reservation/reservation.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ReservationService } from "./reservation.service";
 import { ReservationController } from "./reservation.controller";
+import { AuthModule } from "../auth/auth.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ReservationEntity } from "./entities/reservation.entity";
 
 @Module({
+  imports: [AuthModule, TypeOrmModule.forFeature([ReservationEntity])],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
