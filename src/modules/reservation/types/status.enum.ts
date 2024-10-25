2e97fdef56ac3fdc547e4537e311be6c53f0defa
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 20:17:00 +0330

    add PaymentStatus and ReservationStatus

---
+++ src/modules/reservation/types/status.enum.ts
@@ -0,0 +1,10 @@
+export enum PaymentStatus {
+  Pending = "pending",
+  Confirmed = "confirmed",
+  Reject = "rejected",
+}
+export enum ReservationStatus {
+  Pending = "pending",
+  Confirmed = "confirmed",
+  Reject = "rejected",
+}
