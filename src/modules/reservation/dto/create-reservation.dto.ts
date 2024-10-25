7bc1ff9739d17825c0c4f6efcac5909d7e52cdc9
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 20:16:35 +0330

    create dto

--- src/modules/reservation/dto/create-reservation.dto.ts
+++ src/modules/reservation/dto/create-reservation.dto.ts
@@ -1,1 +1,19 @@
-export class CreateReservationDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { PaymentStatus } from "../types/status.enum";
+
+export class CreateReservationDto {
+  @ApiProperty()
+  clinicId: number;
+  @ApiProperty()
+  userId: number;
+  @ApiProperty()
+  start_visit_time: number;
+  @ApiProperty()
+  finish_visit_time: number;
+  // @ApiProperty()
+  // status: string;
+  @ApiProperty()
+  date: Date;
+  // @ApiProperty({ enum: PaymentStatus})
+  // payment_status: string;
+}
