275b995833bde44d994f42f29ebd00aa532927b9
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 14:20:53 +0330

    import clinicModule and AuthModule

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -3,9 +3,17 @@ import { TransactionsService } from "./transactions.service";
 import { TransactionsController } from "./transactions.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TransactionEntity } from "./entities/transaction.entity";
+import { AuthModule } from "../auth/auth.module";
+import { ClinicService } from "../clinic/clinic.service";
+import { ClinicEntity } from "../clinic/entities/clinic.entity";
+import { ClinicModule } from "../clinic/clinic.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TransactionEntity])],
+  imports: [
+    AuthModule,
+    ClinicModule,
+    TypeOrmModule.forFeature([TransactionEntity, ClinicEntity]),
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
