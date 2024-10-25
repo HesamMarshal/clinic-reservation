7e36c40fadab29f1149842894952042c341cdf25
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 12:56:59 +0330

    add transaction entity

--- src/modules/transactions/entities/transaction.entity.ts
+++ src/modules/transactions/entities/transaction.entity.ts
@@ -1,1 +1,27 @@
-export class Transaction {}
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntityName } from "src/common/enums/entity.enum";
+import { ClinicEntity } from "src/modules/clinic/entities/clinic.entity";
+import { UserEntity } from "src/modules/user/entities/user.entity";
+import { Column, Entity, ManyToMany, ManyToOne } from "typeorm";
+
+@Entity(EntityName.Transacion)
+export class TransactionEntity extends BaseEntity {
+  // Fields
+  @Column()
+  clinicId: number;
+  @Column()
+  userId: number;
+  @Column()
+  amount: number;
+  @Column()
+  status: boolean;
+  @Column()
+  date: number;
+
+  // Relations
+  @ManyToOne(() => ClinicEntity, (clinic) => clinic.id)
+  clinic: ClinicEntity;
+
+  @ManyToOne(() => UserEntity, (user) => user.id)
+  user: UserEntity;
+}
