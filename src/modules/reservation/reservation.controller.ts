207f2aea198cf2ab2558612028f88b405d85610b
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 20:15:39 +0330

    add AuthGuard and Apitags

--- src/modules/reservation/reservation.controller.ts
+++ src/modules/reservation/reservation.controller.ts
@@ -6,16 +6,24 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from "@nestjs/common";
 import { ReservationService } from "./reservation.service";
 import { CreateReservationDto } from "./dto/create-reservation.dto";
 import { UpdateReservationDto } from "./dto/update-reservation.dto";
+import { ApiBearerAuth, ApiConsumes, ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "../auth/guards/auth.guard";
+import { SwaggerConsumes } from "src/common/swagger-consume.enum";
 
 @Controller("reservation")
+@ApiTags("Reservation")
+@ApiBearerAuth("Authorization")
+@UseGuards(AuthGuard)
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
   @Post()
+  @ApiConsumes(SwaggerConsumes.UrlEncoded, SwaggerConsumes.Json)
   create(@Body() createReservationDto: CreateReservationDto) {
     return this.reservationService.create(createReservationDto);
   }
