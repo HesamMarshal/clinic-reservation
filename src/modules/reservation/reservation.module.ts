0cca7a2bc52080cab861e31e519d3910444f81ae
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-10-25 22:04:18 +0330

    add plannerEntity

--- src/modules/reservation/reservation.module.ts
+++ src/modules/reservation/reservation.module.ts
@@ -4,9 +4,13 @@ import { ReservationController } from "./reservation.controller";
 import { AuthModule } from "../auth/auth.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ReservationEntity } from "./entities/reservation.entity";
+import { PlannerEntity } from "../planner/entities/planner.entity";
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([ReservationEntity])],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([ReservationEntity, PlannerEntity]),
+  ],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
